15f032997104d8bb3adebd466d3eaf594b1d9814
Hanien <hanien@localhost.localdomain>
2019-01-08 21:00:44 +0200

    inital commit

---
+++ java/app/src/main/java/com/example/mapdemo/MainActivity.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.mapdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import android.widget.TextView;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Request;
+
+/**
+ * The main activity of the API library demo gallery.
+ * <p>
+ * The main layout lists the demonstrated features, with buttons to launch them.
+ */
+public final class MainActivity extends AppCompatActivity
+        implements AdapterView.OnItemClickListener {
+/**
+    final TextView mTextView = (TextView) findViewById(R.id.text);
+// ...
+
+    public void makePost() {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://staging.raye7.com/android_interns/index";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+ **/
+
+    /**
+     * A custom array adapter that shows a {@link FeatureView} containing details about the demo.
+     */
+    private static class CustomArrayAdapter extends ArrayAdapter<DemoDetails> {
+
+        /**
+         * @param demos An array containing the details of the demos to be displayed.
+         */
+        public CustomArrayAdapter(Context context, DemoDetails[] demos) {
+            super(context, R.layout.feature, R.id.title, demos);
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            FeatureView featureView;
+            if (convertView instanceof FeatureView) {
+                featureView = (FeatureView) convertView;
+            } else {
+                featureView = new FeatureView(getContext());
+            }
+
+            DemoDetails demo = getItem(position);
+
+            featureView.setTitleId(demo.titleId);
+            featureView.setDescriptionId(demo.descriptionId);
+
+            Resources resources = getContext().getResources();
+            String title = resources.getString(demo.titleId);
+            String description = resources.getString(demo.descriptionId);
+            featureView.setContentDescription(title + ". " + description);
+
+            return featureView;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ListView list = (ListView) findViewById(R.id.list);
+
+        ListAdapter adapter = new CustomArrayAdapter(this, DemoDetailsList.DEMOS);
+
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(this);
+        list.setEmptyView(findViewById(R.id.text));
+    //makePost();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        DemoDetails demo = (DemoDetails) parent.getAdapter().getItem(position);
+        startActivity(new Intent(this, demo.activityClass));
+    }
+}
